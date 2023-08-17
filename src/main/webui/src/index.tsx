9348022c95673159bd54bce43b83022175d43a3a
melloware <mellowaredev@gmail.com>
2023-08-17 08:29:39 -0400

    PrimreReact 9.6.2

--- src/main/webui/src/index.tsx
+++ src/main/webui/src/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { HashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { PrimeReactProvider } from 'primereact/api';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 /**
@@ -15,7 +16,9 @@ root.render(
 	<React.StrictMode>
 		<HashRouter>
 			<QueryClientProvider client={queryClient}>
-				<App />
+				<PrimeReactProvider>
+					<App />
+				</PrimeReactProvider>
 				<ReactQueryDevtools initialIsOpen={false} />
 			</QueryClientProvider>
 		</HashRouter>
