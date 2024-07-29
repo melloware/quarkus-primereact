92af4c1730048ecbd4a5b0b8e710a03e008c07a0
melloware <mellowaredev@gmail.com>
2024-07-29 08:38:28 -0400

    PrimeReact 10.8.0

--- src/main/webui/src/service/CarService.ts
+++ src/main/webui/src/service/CarService.ts
@@ -10,6 +10,11 @@ import type { MutationFunction, QueryFunction, QueryKey, UseMutationOptions, Use
 import { useCallback } from 'react';
 import { useAxiosMutator } from './AxiosMutator';
 import type { ErrorType } from './AxiosMutator';
+export type LoggingManagerUpdateBody = {
+	loggerLevel?: LoggerLevel;
+	loggerName?: unknown;
+};
+
 export type LoggingManagerGetAllParams = {
 	loggerName?: string;
 };
@@ -17,6 +22,26 @@ export type LoggingManagerGetAllParams = {
 export type GetEntityCarsParams = {
 	request?: string;
 };
+
+export type HealthCheckStatus = (typeof HealthCheckStatus)[keyof typeof HealthCheckStatus];
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export const HealthCheckStatus = {
+	DOWN: 'DOWN',
+	UP: 'UP'
+} as const;
+
+/**
+ * @nullable
+ */
+export type HealthCheckResponseData = { [key: string]: unknown } | null;
+
+export interface HealthCheckResponse {
+	/** @nullable */
+	data?: HealthCheckResponseData;
+	name?: string;
+	status?: HealthCheckStatus;
+}
 
 export type LoggerLevel = (typeof LoggerLevel)[keyof typeof LoggerLevel];
 
@@ -38,35 +63,10 @@ export const LoggerLevel = {
 	ALL: 'ALL'
 } as const;
 
-export type LoggingManagerUpdateBody = {
-	loggerLevel?: LoggerLevel;
-	loggerName?: unknown;
-};
-
 export interface LoggerInfo {
 	configuredLevel?: LoggerLevel;
 	effectiveLevel?: LoggerLevel;
 	name?: string;
-}
-
-export type HealthCheckStatus = (typeof HealthCheckStatus)[keyof typeof HealthCheckStatus];
-
-// eslint-disable-next-line @typescript-eslint/no-redeclare
-export const HealthCheckStatus = {
-	DOWN: 'DOWN',
-	UP: 'UP'
-} as const;
-
-/**
- * @nullable
- */
-export type HealthCheckResponseData = { [key: string]: unknown } | null;
-
-export interface HealthCheckResponse {
-	/** @nullable */
-	data?: HealthCheckResponseData;
-	name?: string;
-	status?: HealthCheckStatus;
 }
 
 /**
@@ -451,6 +451,176 @@ export const useDeleteEntityCarsId = <TError = ErrorType<unknown>, TContext = un
 };
 
 /**
+ * Get information on all loggers or a specific logger.
+ * @summary Information on Logger(s)
+ */
+export const useLoggingManagerGetAllHook = () => {
+	const loggingManagerGetAll = useAxiosMutator<LoggerInfo[]>();
+
+	return useCallback(
+		(params?: LoggingManagerGetAllParams, signal?: AbortSignal) => {
+			return loggingManagerGetAll({ url: `/q/logging-manager`, method: 'GET', params, signal });
+		},
+		[loggingManagerGetAll]
+	);
+};
+
+export const getLoggingManagerGetAllQueryKey = (params?: LoggingManagerGetAllParams) => {
+	return [`/q/logging-manager`, ...(params ? [params] : [])] as const;
+};
+
+export const useLoggingManagerGetAllQueryOptions = <TData = Awaited<ReturnType<ReturnType<typeof useLoggingManagerGetAllHook>>>, TError = ErrorType<void>>(
+	params?: LoggingManagerGetAllParams,
+	options?: { query?: Partial<UseQueryOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerGetAllHook>>>, TError, TData>> }
+) => {
+	const { query: queryOptions } = options ?? {};
+
+	const queryKey = queryOptions?.queryKey ?? getLoggingManagerGetAllQueryKey(params);
+
+	const loggingManagerGetAll = useLoggingManagerGetAllHook();
+
+	const queryFn: QueryFunction<Awaited<ReturnType<ReturnType<typeof useLoggingManagerGetAllHook>>>> = ({ signal }) => loggingManagerGetAll(params, signal);
+
+	return { queryKey, queryFn, ...queryOptions } as UseQueryOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerGetAllHook>>>, TError, TData> & {
+		queryKey: QueryKey;
+	};
+};
+
+export type LoggingManagerGetAllQueryResult = NonNullable<Awaited<ReturnType<ReturnType<typeof useLoggingManagerGetAllHook>>>>;
+export type LoggingManagerGetAllQueryError = ErrorType<void>;
+
+/**
+ * @summary Information on Logger(s)
+ */
+export const useLoggingManagerGetAll = <TData = Awaited<ReturnType<ReturnType<typeof useLoggingManagerGetAllHook>>>, TError = ErrorType<void>>(
+	params?: LoggingManagerGetAllParams,
+	options?: { query?: Partial<UseQueryOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerGetAllHook>>>, TError, TData>> }
+): UseQueryResult<TData, TError> & { queryKey: QueryKey } => {
+	const queryOptions = useLoggingManagerGetAllQueryOptions(params, options);
+
+	const query = useQuery(queryOptions) as UseQueryResult<TData, TError> & { queryKey: QueryKey };
+
+	query.queryKey = queryOptions.queryKey;
+
+	return query;
+};
+
+/**
+ * Update a log level for a certain logger
+ * @summary Update log level
+ */
+export const useLoggingManagerUpdateHook = () => {
+	const loggingManagerUpdate = useAxiosMutator<void>();
+
+	return useCallback(
+		(loggingManagerUpdateBody: LoggingManagerUpdateBody) => {
+			const formUrlEncoded = new URLSearchParams();
+			if (loggingManagerUpdateBody.loggerName !== undefined) {
+				formUrlEncoded.append('loggerName', loggingManagerUpdateBody.loggerName);
+			}
+			if (loggingManagerUpdateBody.loggerLevel !== undefined) {
+				formUrlEncoded.append('loggerLevel', loggingManagerUpdateBody.loggerLevel);
+			}
+
+			return loggingManagerUpdate({
+				url: `/q/logging-manager`,
+				method: 'POST',
+				headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+				data: formUrlEncoded
+			});
+		},
+		[loggingManagerUpdate]
+	);
+};
+
+export const useLoggingManagerUpdateMutationOptions = <TError = ErrorType<unknown>, TContext = unknown>(options?: {
+	mutation?: UseMutationOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerUpdateHook>>>, TError, { data: LoggingManagerUpdateBody }, TContext>;
+}): UseMutationOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerUpdateHook>>>, TError, { data: LoggingManagerUpdateBody }, TContext> => {
+	const { mutation: mutationOptions } = options ?? {};
+
+	const loggingManagerUpdate = useLoggingManagerUpdateHook();
+
+	const mutationFn: MutationFunction<Awaited<ReturnType<ReturnType<typeof useLoggingManagerUpdateHook>>>, { data: LoggingManagerUpdateBody }> = (props) => {
+		const { data } = props ?? {};
+
+		return loggingManagerUpdate(data);
+	};
+
+	return { mutationFn, ...mutationOptions };
+};
+
+export type LoggingManagerUpdateMutationResult = NonNullable<Awaited<ReturnType<ReturnType<typeof useLoggingManagerUpdateHook>>>>;
+export type LoggingManagerUpdateMutationBody = LoggingManagerUpdateBody;
+export type LoggingManagerUpdateMutationError = ErrorType<unknown>;
+
+/**
+ * @summary Update log level
+ */
+export const useLoggingManagerUpdate = <TError = ErrorType<unknown>, TContext = unknown>(options?: {
+	mutation?: UseMutationOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerUpdateHook>>>, TError, { data: LoggingManagerUpdateBody }, TContext>;
+}): UseMutationResult<Awaited<ReturnType<ReturnType<typeof useLoggingManagerUpdateHook>>>, TError, { data: LoggingManagerUpdateBody }, TContext> => {
+	const mutationOptions = useLoggingManagerUpdateMutationOptions(options);
+
+	return useMutation(mutationOptions);
+};
+
+/**
+ * This returns all possible log levels
+ * @summary Get all available levels
+ */
+export const useLoggingManagerLevelsHook = () => {
+	const loggingManagerLevels = useAxiosMutator<LoggerLevel[]>();
+
+	return useCallback(
+		(signal?: AbortSignal) => {
+			return loggingManagerLevels({ url: `/q/logging-manager/levels`, method: 'GET', signal });
+		},
+		[loggingManagerLevels]
+	);
+};
+
+export const getLoggingManagerLevelsQueryKey = () => {
+	return [`/q/logging-manager/levels`] as const;
+};
+
+export const useLoggingManagerLevelsQueryOptions = <
+	TData = Awaited<ReturnType<ReturnType<typeof useLoggingManagerLevelsHook>>>,
+	TError = ErrorType<unknown>
+>(options?: {
+	query?: Partial<UseQueryOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerLevelsHook>>>, TError, TData>>;
+}) => {
+	const { query: queryOptions } = options ?? {};
+
+	const queryKey = queryOptions?.queryKey ?? getLoggingManagerLevelsQueryKey();
+
+	const loggingManagerLevels = useLoggingManagerLevelsHook();
+
+	const queryFn: QueryFunction<Awaited<ReturnType<ReturnType<typeof useLoggingManagerLevelsHook>>>> = ({ signal }) => loggingManagerLevels(signal);
+
+	return { queryKey, queryFn, ...queryOptions } as UseQueryOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerLevelsHook>>>, TError, TData> & {
+		queryKey: QueryKey;
+	};
+};
+
+export type LoggingManagerLevelsQueryResult = NonNullable<Awaited<ReturnType<ReturnType<typeof useLoggingManagerLevelsHook>>>>;
+export type LoggingManagerLevelsQueryError = ErrorType<unknown>;
+
+/**
+ * @summary Get all available levels
+ */
+export const useLoggingManagerLevels = <TData = Awaited<ReturnType<ReturnType<typeof useLoggingManagerLevelsHook>>>, TError = ErrorType<unknown>>(options?: {
+	query?: Partial<UseQueryOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerLevelsHook>>>, TError, TData>>;
+}): UseQueryResult<TData, TError> & { queryKey: QueryKey } => {
+	const queryOptions = useLoggingManagerLevelsQueryOptions(options);
+
+	const query = useQuery(queryOptions) as UseQueryResult<TData, TError> & { queryKey: QueryKey };
+
+	query.queryKey = queryOptions.queryKey;
+
+	return query;
+};
+
+/**
  * Check the health of the application
  * @summary An aggregated view of the Liveness, Readiness and Startup of this application
  */
@@ -686,176 +856,6 @@ export const useMicroprofileHealthStartup = <
 	query?: Partial<UseQueryOptions<Awaited<ReturnType<ReturnType<typeof useMicroprofileHealthStartupHook>>>, TError, TData>>;
 }): UseQueryResult<TData, TError> & { queryKey: QueryKey } => {
 	const queryOptions = useMicroprofileHealthStartupQueryOptions(options);
-
-	const query = useQuery(queryOptions) as UseQueryResult<TData, TError> & { queryKey: QueryKey };
-
-	query.queryKey = queryOptions.queryKey;
-
-	return query;
-};
-
-/**
- * Get information on all loggers or a specific logger.
- * @summary Information on Logger(s)
- */
-export const useLoggingManagerGetAllHook = () => {
-	const loggingManagerGetAll = useAxiosMutator<LoggerInfo[]>();
-
-	return useCallback(
-		(params?: LoggingManagerGetAllParams, signal?: AbortSignal) => {
-			return loggingManagerGetAll({ url: `/q/logging-manager`, method: 'GET', params, signal });
-		},
-		[loggingManagerGetAll]
-	);
-};
-
-export const getLoggingManagerGetAllQueryKey = (params?: LoggingManagerGetAllParams) => {
-	return [`/q/logging-manager`, ...(params ? [params] : [])] as const;
-};
-
-export const useLoggingManagerGetAllQueryOptions = <TData = Awaited<ReturnType<ReturnType<typeof useLoggingManagerGetAllHook>>>, TError = ErrorType<void>>(
-	params?: LoggingManagerGetAllParams,
-	options?: { query?: Partial<UseQueryOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerGetAllHook>>>, TError, TData>> }
-) => {
-	const { query: queryOptions } = options ?? {};
-
-	const queryKey = queryOptions?.queryKey ?? getLoggingManagerGetAllQueryKey(params);
-
-	const loggingManagerGetAll = useLoggingManagerGetAllHook();
-
-	const queryFn: QueryFunction<Awaited<ReturnType<ReturnType<typeof useLoggingManagerGetAllHook>>>> = ({ signal }) => loggingManagerGetAll(params, signal);
-
-	return { queryKey, queryFn, ...queryOptions } as UseQueryOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerGetAllHook>>>, TError, TData> & {
-		queryKey: QueryKey;
-	};
-};
-
-export type LoggingManagerGetAllQueryResult = NonNullable<Awaited<ReturnType<ReturnType<typeof useLoggingManagerGetAllHook>>>>;
-export type LoggingManagerGetAllQueryError = ErrorType<void>;
-
-/**
- * @summary Information on Logger(s)
- */
-export const useLoggingManagerGetAll = <TData = Awaited<ReturnType<ReturnType<typeof useLoggingManagerGetAllHook>>>, TError = ErrorType<void>>(
-	params?: LoggingManagerGetAllParams,
-	options?: { query?: Partial<UseQueryOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerGetAllHook>>>, TError, TData>> }
-): UseQueryResult<TData, TError> & { queryKey: QueryKey } => {
-	const queryOptions = useLoggingManagerGetAllQueryOptions(params, options);
-
-	const query = useQuery(queryOptions) as UseQueryResult<TData, TError> & { queryKey: QueryKey };
-
-	query.queryKey = queryOptions.queryKey;
-
-	return query;
-};
-
-/**
- * Update a log level for a certain logger
- * @summary Update log level
- */
-export const useLoggingManagerUpdateHook = () => {
-	const loggingManagerUpdate = useAxiosMutator<void>();
-
-	return useCallback(
-		(loggingManagerUpdateBody: LoggingManagerUpdateBody) => {
-			const formUrlEncoded = new URLSearchParams();
-			if (loggingManagerUpdateBody.loggerName !== undefined) {
-				formUrlEncoded.append('loggerName', loggingManagerUpdateBody.loggerName);
-			}
-			if (loggingManagerUpdateBody.loggerLevel !== undefined) {
-				formUrlEncoded.append('loggerLevel', loggingManagerUpdateBody.loggerLevel);
-			}
-
-			return loggingManagerUpdate({
-				url: `/q/logging-manager`,
-				method: 'POST',
-				headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-				data: formUrlEncoded
-			});
-		},
-		[loggingManagerUpdate]
-	);
-};
-
-export const useLoggingManagerUpdateMutationOptions = <TError = ErrorType<unknown>, TContext = unknown>(options?: {
-	mutation?: UseMutationOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerUpdateHook>>>, TError, { data: LoggingManagerUpdateBody }, TContext>;
-}): UseMutationOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerUpdateHook>>>, TError, { data: LoggingManagerUpdateBody }, TContext> => {
-	const { mutation: mutationOptions } = options ?? {};
-
-	const loggingManagerUpdate = useLoggingManagerUpdateHook();
-
-	const mutationFn: MutationFunction<Awaited<ReturnType<ReturnType<typeof useLoggingManagerUpdateHook>>>, { data: LoggingManagerUpdateBody }> = (props) => {
-		const { data } = props ?? {};
-
-		return loggingManagerUpdate(data);
-	};
-
-	return { mutationFn, ...mutationOptions };
-};
-
-export type LoggingManagerUpdateMutationResult = NonNullable<Awaited<ReturnType<ReturnType<typeof useLoggingManagerUpdateHook>>>>;
-export type LoggingManagerUpdateMutationBody = LoggingManagerUpdateBody;
-export type LoggingManagerUpdateMutationError = ErrorType<unknown>;
-
-/**
- * @summary Update log level
- */
-export const useLoggingManagerUpdate = <TError = ErrorType<unknown>, TContext = unknown>(options?: {
-	mutation?: UseMutationOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerUpdateHook>>>, TError, { data: LoggingManagerUpdateBody }, TContext>;
-}): UseMutationResult<Awaited<ReturnType<ReturnType<typeof useLoggingManagerUpdateHook>>>, TError, { data: LoggingManagerUpdateBody }, TContext> => {
-	const mutationOptions = useLoggingManagerUpdateMutationOptions(options);
-
-	return useMutation(mutationOptions);
-};
-
-/**
- * This returns all possible log levels
- * @summary Get all available levels
- */
-export const useLoggingManagerLevelsHook = () => {
-	const loggingManagerLevels = useAxiosMutator<LoggerLevel[]>();
-
-	return useCallback(
-		(signal?: AbortSignal) => {
-			return loggingManagerLevels({ url: `/q/logging-manager/levels`, method: 'GET', signal });
-		},
-		[loggingManagerLevels]
-	);
-};
-
-export const getLoggingManagerLevelsQueryKey = () => {
-	return [`/q/logging-manager/levels`] as const;
-};
-
-export const useLoggingManagerLevelsQueryOptions = <
-	TData = Awaited<ReturnType<ReturnType<typeof useLoggingManagerLevelsHook>>>,
-	TError = ErrorType<unknown>
->(options?: {
-	query?: Partial<UseQueryOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerLevelsHook>>>, TError, TData>>;
-}) => {
-	const { query: queryOptions } = options ?? {};
-
-	const queryKey = queryOptions?.queryKey ?? getLoggingManagerLevelsQueryKey();
-
-	const loggingManagerLevels = useLoggingManagerLevelsHook();
-
-	const queryFn: QueryFunction<Awaited<ReturnType<ReturnType<typeof useLoggingManagerLevelsHook>>>> = ({ signal }) => loggingManagerLevels(signal);
-
-	return { queryKey, queryFn, ...queryOptions } as UseQueryOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerLevelsHook>>>, TError, TData> & {
-		queryKey: QueryKey;
-	};
-};
-
-export type LoggingManagerLevelsQueryResult = NonNullable<Awaited<ReturnType<ReturnType<typeof useLoggingManagerLevelsHook>>>>;
-export type LoggingManagerLevelsQueryError = ErrorType<unknown>;
-
-/**
- * @summary Get all available levels
- */
-export const useLoggingManagerLevels = <TData = Awaited<ReturnType<ReturnType<typeof useLoggingManagerLevelsHook>>>, TError = ErrorType<unknown>>(options?: {
-	query?: Partial<UseQueryOptions<Awaited<ReturnType<ReturnType<typeof useLoggingManagerLevelsHook>>>, TError, TData>>;
-}): UseQueryResult<TData, TError> & { queryKey: QueryKey } => {
-	const queryOptions = useLoggingManagerLevelsQueryOptions(options);
 
 	const query = useQuery(queryOptions) as UseQueryResult<TData, TError> & { queryKey: QueryKey };
 
