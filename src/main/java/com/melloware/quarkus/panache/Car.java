05db61258a423ebd08fa8d7bd6588981d84c775a
melloware <mellowaredev@gmail.com>
2025-03-25 16:27:47 -0400

    Fix #407: Switch to TanStack Forms

--- src/main/java/com/melloware/quarkus/panache/Car.java
+++ src/main/java/com/melloware/quarkus/panache/Car.java
@@ -11,9 +11,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Version;
+import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
@@ -25,23 +29,29 @@ import lombok.NoArgsConstructor;
 @Schema(name = "Car", description = "Entity that represents a car.")
 public class Car extends PanacheEntity {
 
-	@Column(length = 40, unique = true)
+	@Column(unique = true)
 	@NotBlank(message = "VIN may not be blank")
+	@Size(max=17, message = "VIN may not be more than 17 characters")
 	@Schema(required = true, examples = {"WVGEF9BP4DD085048"}, description = "VIN number")
 	public String vin;
 	@NotBlank(message = "Make may not be blank")
+	@Size(max=255, message = "Make may not be more than 255 characters")
 	@Schema(required = true, examples = {"BMW"}, description = "Manufacturer")
 	public String make;
 	@NotBlank(message = "Model may not be blank")
+	@Size(max=255, message = "Model may not be more than 255 characters")
 	@Schema(required = true, examples = {"330ix"}, description = "Model Number")
 	public String model;
 	@Min(value = 1960)
+	@Max(value = 2050)
 	@Schema(required = true, examples = {"1974"}, description = "Year of manufacture")
 	public int year;
 	@NotBlank(message = "Color may not be blank")
+	@Size(max=20, message = "Color may not be more than 20 characters")
 	@Schema(required = true, examples = {"891d4c"}, description = "HTML color of the car")
 	public String color;
 	@DecimalMin(value = "0.00")
+	@DecimalMax(value = "250000.00")
 	@Schema(required = true, examples = {"9999.99"}, description = "Price")
 	public BigDecimal price;
 
--- src/main/webui/orval.config.ts
+++ src/main/webui/orval.config.ts
@@ -1,23 +1,41 @@
 module.exports = {
-  cars: {
-    output: {
-      target: 'src/service/CarService.ts',
-      client: 'react-query',
-      mock: false,
-      prettier: false,
-      override: {
-        useDates: true,
-        mutator: {
-          path: 'src/service/AxiosMutator.ts',
-          name: 'useAxiosMutator',
-        },
-        query: {
-          useQuery: true,
-        }
-      }
-    },
-    input: {
-      target: './openapi.yaml',
-    },
-  },
+	cars: {
+		output: {
+			target: 'src/service/CarService.ts',
+			client: 'react-query',
+			mock: false,
+			prettier: false,
+			override: {
+				useDates: true,
+				mutator: {
+					path: 'src/service/AxiosMutator.ts',
+					name: 'useAxiosMutator'
+				},
+				query: {
+					useQuery: true
+				}
+			}
+		},
+		input: {
+			target: './openapi.yaml'
+		}
+	},
+	carsZod: {
+		output: {
+			client: 'zod',
+			target: 'src/service/CarService.zod.ts',
+			override: {
+				useDates: true,
+				zod: {
+					coerce: {
+						response: ['date'],
+						body: ['date']
+					}
+				}
+			}
+		},
+		input: {
+			target: './openapi.yaml'
+		}
+	}
 };
